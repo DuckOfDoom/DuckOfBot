f2bddc390f61a056dd4a4ba85eefc2df94e84f0f
Владимир Яновский <vladimir.yanovsky@towardsmars.com>
2016-07-16 20:22:01 +0300

    Yay! Polymorphic responses!

--- src/API/Requests.hs
+++ src/API/Requests.hs
@@ -4,20 +4,16 @@
 module API.Requests where
 
 import           Control.Lens ((^.))
-import           Data.Aeson   (eitherDecode)
+import           Data.Aeson   (eitherDecode, FromJSON)
 import           JSON.Objects
 import           Network.Wreq (get, responseBody)
 import qualified Util.URL      as U
 
--- TODO: Figure out how to make polymorphic request function. This doesnt work =(
---makeRequest :: (FromJSON a) => String -> IO (Either String (Response a))
---makeRequest url = let maybeToEither e = maybe (Left e) Right
---                      in do
---                        response <- get url
---                        return $ maybeToEither ("Request to '" ++ url ++ "' failed") (decode (response ^. responseBody) :: Maybe (Response a))
+makeRequest :: (FromJSON a) => String -> IO (Either String (Response a))
+makeRequest url = do
+  response <- get url
+  return (eitherDecode (response ^. responseBody) :: (FromJSON a) => Either String (Response a))
 
 getMe :: IO (Either String (Response User))
-getMe = do
-  url <- U.getMeUrl
-  r <- get url
-  return (eitherDecode (r ^. responseBody) :: Either String (Response User))
+getMe = U.getMeUrl >>= makeRequest 
+
