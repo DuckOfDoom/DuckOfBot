fcd92c659128662f3ee24d6a20a2ae8722ad7021
Владимир Яновский <vladimir.yanovsky@towardsmars.com>
2016-08-06 15:05:44 +0300

    Fix duplicate error message when token is not present

--- src/Main.hs
+++ src/Main.hs
@@ -5,13 +5,15 @@ import           API.Requests
 import           API.Types
 
 import           Bot
-import           Data.Maybe   (fromJust)
+import           Control.Monad (when)
+import           Data.Either   (isRight)
+import           Data.Maybe    (fromJust)
 
 main :: IO ()
 main = do
   putStrLn "Hello there! Let me check something..."
   me <- getMe
   putStrLn $ printMe me
-  startGetUpdatesLoopWithDelay 0.1
+  when (isRight me) (startGetUpdatesLoopWithDelay 0.1)
   where printMe (Left e) = "Whoops! Something went wrong: '" ++ e ++ "'"
         printMe (Right (Response _ user _ _ )) = "All is good! Nice to meet you! My name is: " ++ firstName (fromJust user)
