98a0a297164804144bc94af9c0c9bbc835a59936
Владимир Яновский <vladimir.yanovsky@towardsmars.com>
2016-08-08 11:42:37 +0300

    Fix warnings

--- src/Util/Time.hs
+++ src/Util/Time.hs
@@ -4,7 +4,6 @@ module Util.Time where
 
 import           Data.Maybe
 import           Data.UTC
-import           Debug.Trace
 import           Text.Printf (printf)
 
 data Timespan = Timespan { days    :: Integer
@@ -17,11 +16,11 @@ instance Show Timespan where
   show t = printf "%dд %dч %dм %dс" (days t) (hours t) (minutes t) (seconds t)
 
 fromSeconds :: Integer -> Timespan
-fromSeconds seconds = Timespan d h m s
-  where d = seconds `div` 86400
-        h = (seconds - d * 86400) `div` 3600
-        m = (seconds - d * 86400 - h * 3600) `div` 60
-        s = seconds - d * 86400 - h * 3600 - m * 60
+fromSeconds x = Timespan d h m s
+  where d = x `div` 86400
+        h = (x - d * 86400) `div` 3600
+        m = (x - d * 86400 - h * 3600) `div` 60
+        s = x - d * 86400 - h * 3600 - m * 60
 
 getCurrentTime :: IO DateTime
 getCurrentTime = getUnixTime :: IO DateTime
@@ -35,7 +34,7 @@ makeDateTime y m d h mi sec = fromJust $ setYear y (epoch :: DateTime)
                                                 >>= setSecond sec
 
 subtractDT :: DateTime -> DateTime -> Timespan
-subtractDT fst snd = fromSeconds (dateTimeToSeconds fst - dateTimeToSeconds snd)
+subtractDT t1 t2 = fromSeconds (dateTimeToSeconds t1 - dateTimeToSeconds t2)
 
 dateTimeToSeconds :: DateTime -> Integer
 dateTimeToSeconds dt = truncate (unixSeconds dt) :: Integer
